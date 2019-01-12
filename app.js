20e673cac9052e9831165fd0e5593270581de352
ralphbarac <r.barac@live.ca>
2019-01-11 22:59:24 -0500

    Rearranged styling, added date from API

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 window.addEventListener('load', ()=> {
     let long;
     let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
-    let temperatureSection = document.querySelector('.temperature');
-    const temperatureSpan = document.querySelector('.temperature span');
+    let timeSectionHour = document.querySelector('.time-section-hour');
+    let timeSectionDate = document.querySelector('.time-section-date');
+    let temperatureDescription = document.querySelector('.temperature-section-summary');
+    let temperatureDegree = document.querySelector('.temperature-section-degree');
+    let locationTimezone = document.querySelector('.temperature-section-location');
+    let temperatureSection = document.querySelector('.temperature-section');
+    const temperatureSpan = document.querySelector('.temperature-section span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position =>{
@@ -28,16 +30,31 @@ window.addEventListener('load', ()=> {
                     locationTimezone.textContent = data.timezone;
                     //Temperature conversion between C/F
                     let celsius = (temperature - 32) * (5 / 9);
+                    //Time conversion from UNIX to current date and time
+                    var date = new Date(data.currently.time*1000);
+                    var modifier = 'AM'
+                    var hours = date.getHours();
+                    if(hours > 12){
+                        hours = hours - 12;
+                        modifier = 'PM'
+                    }
+                    var minutes = "0" + date.getMinutes();
+                    var formattedTime = hours + ':' + minutes.substr(-2);
+                    var formattedDate = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
+                    console.log(formattedTime + modifier);
+                    //Set DOM elements with time from API
+                    timeSectionHour.textContent = formattedTime + modifier
+                    timeSectionDate.textContent = formattedDate;
                     //Set Icon
                     setIcons(icon, document.querySelector('.icon'));
                     //Change Temperature to Celsius/Farenheit
                     temperatureSection.addEventListener('click', () =>{
-                        if(temperatureSpan.textContent === "F"){
-                            temperatureSpan.textContent = "C";
+                        if(temperatureSpan.textContent === "Farenheit"){
+                            temperatureSpan.textContent = "Celsius";
                             temperatureDegree.textContent = Math.floor(celsius);
                         }
                         else{
-                            temperatureSpan.textContent = "F";
+                            temperatureSpan.textContent = "Farenheit";
                             temperatureDegree.textContent = temperature;
                         }
                     });
